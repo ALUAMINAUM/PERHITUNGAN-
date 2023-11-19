240073ba2f1e6b323e488049b37a566cb0f18034
ALUAMINAUM <149672953+ALUAMINAUM@users.noreply.github.com>
2023-11-19 17:11:17 +0700

    Create MATEMATIKA.py

---
+++ MATEMATIKA.py
@@ -0,0 +1,27 @@
+import os
+import sys
+import time
+import random
+import socket
+
+os.system("clear")
+print "Sedang Loading Database..."
+time.sleep(4)
+print "Menjalankan Sistem Operasi..."
+time.sleep(4)
+os.system("clear")
+os.system("figlet Matematika Linux")
+print "===================================="
+print "= AUTHOR  = BY O.Bc/MATAELANG/A.Km ="
+print "= TEAM    = HACKING RI KULONPROGO  ="
+print "= SC      = ALUAMINAUM/Matematika  ="
+print "===================================="
+
+Loading = raw_input("Ketik Lanjut Untuk Memulai= ")
+time.sleep(4)
+input = raw_input("Masukan Key Contoh 5 Kali 13 = ")
+print "Hasil Penjumlahan = 65"
+
+exit = raw_input("Ketik Yes Untuk Keluar= ")
+time.sleep(10)
+os.system("clear")
